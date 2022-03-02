8c2cac02b11fac64cd70efd2db911d5c45146c6d
Anthony Maringo <alm4cu@virginia.edu>
2022-03-02 10:54:50 -0500

    Basic file uploading

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home')
+def hello_page():  # put application's code here
+    return render_template('home.html')
+
+
+@app.route('/camelot')
+def camelot_page():
+    return render_template('camelot.html')
+
+
+@app.route('/camelot_upload', methods=['GET', 'POST'])
+def camelot_upload_file():
+    if request.method == "POST":
+        f = request.files['file']
+        f.save(f.filename)
+        return 'file uploaded successfully'
+
+
+# Camelot keys
+# We need to be able to upload a file
+# Modify that file
+# present new file with the modifications
 
 
 if __name__ == '__main__':
