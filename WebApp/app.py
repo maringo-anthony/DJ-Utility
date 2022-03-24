fb1dac33b229feb8e9e50fd1442695719657695d
Anthony Maringo <alm4cu@virginia.edu>
2022-03-24 18:51:47 -0400

    Add Compression Option for downloading rekordbox.xml

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,3 +1,5 @@
+from zipfile import ZipFile
+
 from flask import Flask, render_template, request, send_file
 
 from WebApp.CamelotKeyConverter import CamelotKeyConverter
@@ -19,15 +21,25 @@ def camelot_upload_file():
 
         converter = CamelotKeyConverter()
         converted_xml = converter.convertToCamelotKeys()
+
         with open("rekordbox_out.xml", "w") as out_file:
             out_file.write(converted_xml)
+
+        if request.form['compressed_radio']:
+            with ZipFile("rekordbox.zip", "w") as zipObj:
+                zipObj.write("rekordbox_out.xml")
 
         return render_template('converted_xml.html', xml=converted_xml)
 
 
-@app.route('/download')
+@app.route('/download_xml')
 def download_rekordbox_xml():
     return send_file('rekordbox.xml', as_attachment=True)
+
+
+@app.route('/download_compressed_xml')
+def download_compressed_rekordbox_xml():
+    return send_file('rekordbox.zip', as_attachment=True)
 
 
 if __name__ == '__main__':
