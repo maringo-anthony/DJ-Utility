72e3cfd757cb90e5a3ac1dd50fadbfd3d35ea7c9
Anthony Maringo <alm4cu@virginia.edu>
2022-03-24 22:02:22 -0400

    Framework for youtube search feature

---
+++ WebApp/YoutubeDownloader.py
@@ -0,0 +1,8 @@
+# Anthony Maringo Alm4cu
+class Youtube_Downloader:
+
+    def find_song(self):
+        return False
+
+    def download_song(self, song_name):
+        return "path/to/song/"
--- WebApp/app.py
+++ WebApp/app.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 from flask import Flask, render_template, request, send_file
 
 from WebApp.CamelotKeyConverter import CamelotKeyConverter
+from WebApp.YoutubeDownloader import Youtube_Downloader
 
 app = Flask(__name__)
 
@@ -17,6 +18,9 @@ def camelot_page():
 def camelot_upload_file():
     if request.method == "POST":
         f = request.files['file']
+        if not f:
+            return render_template('camelot.html')
+
         f.save(f.filename)
 
         converter = CamelotKeyConverter()
@@ -40,6 +44,27 @@ def download_rekordbox_xml():
 @app.route('/download_compressed_xml')
 def download_compressed_rekordbox_xml():
     return send_file('rekordbox.zip', as_attachment=True)
+
+
+@app.route('/youtube_search')
+def youtube_search_page():
+    return render_template("youtube_search.html")
+
+
+@app.route('/youtube_search', methods=['GET', 'POST'])
+def download_mp3():
+    if request.method == "POST":
+        song_name = request.form['song-name']
+
+        downloader = Youtube_Downloader()
+        song_file_name = downloader.download_song(song_name)
+        return render_template('song_download.html')
+
+
+@app.route('/song_download')
+def download_song():
+    # TODO: Find song that was downloaded then send it
+    return send_file('Dance_With_Me_Tonight.mp3', as_attachment=True)
 
 
 if __name__ == '__main__':
